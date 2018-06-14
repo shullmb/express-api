c98c19544305bc973f399c8b0806f46ebe093e87
shullmb <shullmb@gmail.com>
2018-06-14 11:12:59 -0700

    added view and jquery for update with PUT

---
+++ static/js/script.js
@@ -0,0 +1,17 @@
+$(document).ready( function() {
+    console.log('working');
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        var newData = $(this).serialize();
+        var url = $(this).attr('action');
+        $.ajax({
+            method: 'PUT',
+            url: url,
+            data: newData
+        }).done( function(data) {
+            console.log(data);
+            window.location = '/bikes'
+        })
+    })
+})
+
